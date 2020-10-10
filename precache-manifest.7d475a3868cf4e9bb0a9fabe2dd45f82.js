ff1342f36b978bd622836303b12fa9a31e02a35a
qwersteve07 <qwersteve07@gmail.com>
2020-10-11 03:35:20 +0800

    Updates

---
+++ precache-manifest.7d475a3868cf4e9bb0a9fabe2dd45f82.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ec493e348cfce73264efd065949ff1",
+    "url": "/JH-website/index.html"
+  },
+  {
+    "revision": "979a4676d6cf8b50cde0",
+    "url": "/JH-website/static/css/main.b8c3214c.chunk.css"
+  },
+  {
+    "revision": "e60d628ddaeb0d05ec32",
+    "url": "/JH-website/static/js/2.fd1bf804.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JH-website/static/js/2.fd1bf804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979a4676d6cf8b50cde0",
+    "url": "/JH-website/static/js/main.70750d79.chunk.js"
+  },
+  {
+    "revision": "f895d4377d30a422fef2",
+    "url": "/JH-website/static/js/runtime-main.9bbe5d4d.js"
+  },
+  {
+    "revision": "59c47a3d3e3d846096d49e1fa520e579",
+    "url": "/JH-website/static/media/crystalline-glucose.59c47a3d.jpg"
+  },
+  {
+    "revision": "84b30201f246d593c80ca2f459ad40ef",
+    "url": "/JH-website/static/media/fructose.84b30201.jpg"
+  },
+  {
+    "revision": "c983d8f97131f1b73fc23461e5a7fe18",
+    "url": "/JH-website/static/media/maltose-75.c983d8f9.jpg"
+  },
+  {
+    "revision": "b32d1f2407786158fc9f511855fec2ff",
+    "url": "/JH-website/static/media/maltose-85-25.b32d1f24.jpg"
+  },
+  {
+    "revision": "0ee41d37e020a91ee568d156b083f2d9",
+    "url": "/JH-website/static/media/maltose-85-30.0ee41d37.jpg"
+  },
+  {
+    "revision": "3ddf7a21699e4534fcf4294124c348c3",
+    "url": "/JH-website/static/media/maltose.3ddf7a21.jpg"
+  },
+  {
+    "revision": "e401d92d6758fb2eda27450e8c1e30f1",
+    "url": "/JH-website/static/media/powdered-glucose.e401d92d.jpg"
+  },
+  {
+    "revision": "c90af09afd3f6f5e1b0d4fc9553fc0e0",
+    "url": "/JH-website/static/media/refined-glucose.c90af09a.jpg"
+  },
+  {
+    "revision": "292d6e9f0f65ae25cb667a30631807da",
+    "url": "/JH-website/static/media/sucrose.292d6e9f.jpg"
+  }
+]);
--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-export const PATH = Object.freeze({
-  ABOUT: '/about',
-  PRODUCT: '/product',
-  PROCESS: '/process',
-  QUESTION: '/question',
-  CONTACT: '/contact',
-  TRAFFIC: '/traffic',
-});
